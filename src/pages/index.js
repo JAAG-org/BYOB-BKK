a4759bd33f1f240813b70804a8fc5a09cdea881b
dodadoa <maddodadoa@gmail.com>
2023-05-23 18:08:47 +0700

    change theme

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,31 +2,31 @@ import * as React from "react";
 
 const IndexPage = () => {
   return (
-    <div className="bg-black h-full ">
-      <nav className="w-full h-20 p-6 font-mono">
+    <div className="bg-slate-300">
+      <nav className="w-full h-20 p-6 font-mono drop-shadow-xl shadow-black">
         <div className="flex flex-row justify-between w-full">
-          <a className="text-white ml-2 text-xl hover:cursor-pointer hover:underline hover:underline-offset-4">
+          <a className="text-white ml-2 text-xl hover:cursor-pointer hover:underline hover:underline-offset-4 hover:text-green-600">
             BYOB
           </a>
           <div className="flex flex-row text-xl">
-            <a className="text-white mx-2 hover:cursor-pointer hover:underline hover:underline-offset-4">
+            <a className="text-white mx-2 hover:cursor-pointer hover:underline hover:underline-offset-4 hover:text-green-600">
               Participants
             </a>
-            <a className="text-white mx-2 hover:cursor-pointer hover:underline hover:underline-offset-4">
+            <a className="text-white mx-2 hover:cursor-pointer hover:underline hover:underline-offset-4 hover:text-green-600">
               Venue
             </a>
-            <a className="text-white mx-2 hover:cursor-pointer hover:underline hover:underline-offset-4">
+            <a className="text-white mx-2 hover:cursor-pointer hover:underline hover:underline-offset-4 hover:text-green-600">
               Sponsors
             </a>
           </div>
         </div>
       </nav>
       <main className="p-10 h-full text-center">
-        <h1 className="font-display text-4xl text-white my-2">
+        <h1 className="font-display text-4xl text-white drop-shadow-4xl shadow-white my-2">
           BRING YOUR OWN BEAMER
         </h1>
-        <h1 className="font-display text-4xl text-white my-2">
-          BANGKOK EDITION
+        <h1 className="font-display text-4xl text-white drop-shadow-4xl shadow-white my-2">
+          1.07.2023
         </h1>
       </main>
     </div>
