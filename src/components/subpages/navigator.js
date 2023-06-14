0b3bf82bcfb4e8030fe6c5bb958a4fababe659ea
dodadoa <maddodadoa@gmail.com>
2023-06-15 00:21:08 +0700

    finishing the web

--- src/components/subpages/navigator.js
+++ src/components/subpages/navigator.js
@@ -1,6 +1,10 @@
 import * as React from "react";
+import { useState } from "react";
+import { XCircleIcon } from "@heroicons/react/24/solid";
 
 const Navigator = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <nav className="w-full h-20 p-6 font-mono drop-shadow-xl shadow-black">
       <div className="flex flex-row justify-between w-full">
@@ -10,13 +14,19 @@ const Navigator = () => {
         >
           BYOB
         </a>
-        <div className="flex-row text-xl hidden lg:flex">
+        <div className="flex-row text-xl gap-4 hidden lg:flex">
           {/* <a
             href="#participants"
             className="text-white mx-2 hover:cursor-pointer hover:underline hover:underline-offset-4 hover:text-green-600"
           >
             Participants
           </a> */}
+          <a
+            href="#opencalls"
+            className="text-white mx-2 hover:cursor-pointer hover:underline hover:underline-offset-4 hover:text-green-600"
+          >
+            Open calls
+          </a>
           <a
             href="#venue"
             className="text-white mx-2 hover:cursor-pointer hover:underline hover:underline-offset-4 hover:text-green-600"
@@ -43,13 +53,63 @@ const Navigator = () => {
           </a> */}
         </div>
 
-        <div class="flex hover:cursor-pointer hover:animate-pulse hover:ease-linear	lg:hidden">
+        <div
+          class="flex hover:cursor-pointer hover:animate-pulse hover:ease-linear	lg:hidden"
+          onClick={() => setIsMenuOpen(true)}
+        >
           <div class="space-y-2">
             <span class="block w-8 h-0.5 bg-white"></span>
             <span class="block w-8 h-0.5 bg-white"></span>
             <span class="block w-8 h-0.5 bg-white"></span>
           </div>
         </div>
+        {isMenuOpen && (
+          <div className="flex flex-row justify-between absolute top-0 right-0 bg-gray-600 w-full p-6 items-end shadow-xl">
+            <XCircleIcon
+              className="hover:cursor-pointer hover:animate-pulse hover:ease-linear"
+              fill="white"
+              width={38}
+              onClick={() => setIsMenuOpen(false)}
+            />
+            <div className="flex flex-col gap-4">
+              <a
+                href="#participants"
+                className="text-white hover:underline hover:underline-offset-4 hover:text-green-600"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                Participants
+              </a>
+              <a
+                href="#opencalls"
+                className="text-white hover:underline hover:underline-offset-4 hover:text-green-600"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                Open calls
+              </a>
+              <a
+                href="#venue"
+                className="text-white hover:underline hover:underline-offset-4 hover:text-green-600"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                Venue
+              </a>
+              <a
+                href="#organizers"
+                className="text-white hover:underline hover:underline-offset-4 hover:text-green-600"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                Organizers
+              </a>
+              <a
+                href="#FAQS"
+                className="text-white hover:underline hover:underline-offset-4 hover:text-green-600"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                FAQS
+              </a>
+            </div>
+          </div>
+        )}
       </div>
     </nav>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Disclosure from "../components/atoms/disclosure";
 import Navigator from "../components/subpages/navigator";
 import { StaticImage } from "gatsby-plugin-image";
 import GoogleMapReact from "google-map-react";
+import { Link } from "gatsby";
 import { useTranslation } from "gatsby-plugin-react-i18next";
 
 const IndexPage = () => {
@@ -30,6 +31,52 @@ const IndexPage = () => {
           </div>
         </article> */}
 
+        <article id="opencalls" className="mt-20 p-10">
+          <p className="font-mono font-medium text-lg">
+            BYOB is a series of motion picture exhibitions that take place for
+            one night in various locations around the world. Artists will bring
+            the projector/beamer to display their works along with others filled
+            in the space to create the visual together.
+          </p>
+          <p className="font-mono font-medium text-lg mt-8">
+            BYOB was originally started by Rafaël Rozendaal in Berlin, Germany,
+            in 2010. This event showcasing moving images has been expanded by
+            individuals interested in motion media in many countries, such as
+            Amsterdam, Los Angeles, Melbourne, and Taipei. The open invitation
+            invites visual artists and creators to showcase their work,
+            exploring new possibilities in presenting images through projection.
+          </p>
+        </article>
+
+        <article
+          id="opencalls"
+          className="mt-20 p-10 border border-white rounded-xl shadow-md shadow-gray"
+        >
+          <div className="flex flex-row">
+            <h2 className="font-display text-4xl text-white drop-shadow-4xl shadow-white my-2">
+              {t("opencalls")}
+            </h2>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="h-full">
+              <StaticImage
+                src="../images/opencall_poster.jpg"
+                alt="Open Calls Poster"
+                height={600}
+              />
+            </div>
+            <div className="mt-8">
+              <a
+                className="font-mono text-2xl text-blue-700 font-light shadow-lg p-4 px-8 rounded border hover:shadow-white hover:shadow-xl border-white hover:cursor-pointer w-80"
+                href="https://docs.google.com/forms/d/e/1FAIpQLScHKDI9dAQuY9tdlcGs8fBDFwVo25c1epdl0HMF5xArAMNlpg/viewform"
+              >
+                {" "}
+                Register here !{" "}
+              </a>
+            </div>
+          </div>
+        </article>
+
         <article
           id="venue"
           className="mt-20 p-10 border border-white rounded-xl shadow-md shadow-gray"
@@ -42,7 +89,10 @@ const IndexPage = () => {
 
           <div className="mt-8 flex lg:flex-row flex-col items-center gap-10 lg:justify-around">
             <div className="w-96 h-full">
-              <StaticImage src="../images/bccg.svg" alt="venue" />
+              <StaticImage
+                src="../images/bccg.svg"
+                alt="BANGKOK CITYCITY GALLERY"
+              />
             </div>
             <div className="w-96 h-[20rem]">
               <GoogleMapReact
@@ -60,7 +110,7 @@ const IndexPage = () => {
                   lat={13.7236455}
                   lng={100.544534}
                 >
-                  Bangkok Citycity Gallery
+                  BANGKOK CITYCITY GALLERY
                 </div>
               </GoogleMapReact>
             </div>
@@ -100,6 +150,59 @@ const IndexPage = () => {
               Organizers
             </h2>
           </div>
+          <div className="flex flex-col w-full lg:flex-row lg:justify-around gap-5">
+            <div className="flex flex-col gap-5 border border-gray shadow-sm pr-4 rounded">
+              <h2 className="font-mono text-4xl font-semibold text-white drop-shadow-4xl shadow-white my-2">
+                JAAG
+              </h2>
+              <div className="flex lg:flex-row lg:justify-between lg:items-start flex-col items-center gap-6">
+                <StaticImage
+                  width={300}
+                  height={400}
+                  src="../images/JAAG.jpg"
+                  alt="JAAG (Joint Artistic Amateur Group"
+                />
+                <p className="font-mono w-96 lg:text-start text-center">
+                  JAAG was founded in 2021 by Wasawat Somno (live-coding artist,
+                  software engineer), Tewprai Bualoi (visual artist, software
+                  engineer), and Nanut Thanapornrapee (visual artist, creative).
+                  Their transdisciplinary practices focuses on visual and
+                  technological interventions. They are actively involved in
+                  VJing in clubs and experimental music venues, while also
+                  seeking to expand their vision and thesis across AI,
+                  generative visuals, live-coding, and 3D rendering. (Photo:
+                  Nina Sach)
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-col gap-5 border border-gray shadow-sm pr-4 rounded">
+              <h2 className="font-mono text-4xl font-semibold text-white drop-shadow-4xl shadow-white my-2">
+                ZonZon.Studio
+              </h2>
+              <div className="flex lg:flex-row lg:justify-between lg:items-start flex-col items-center gap-6">
+                <StaticImage
+                  width={300}
+                  height={400}
+                  src="../images/ZonZon.jpg"
+                  alt="ZonZon.Studio"
+                />
+                <p className="font-mono w-96 lg:text-start text-center">
+                  Setthasiri Chanjaradpong aka ZonZon.Studio is a keen observer
+                  who expresses himself through video art and documentaries.
+                  Setthasiri has moved from the field of filmmaking to the
+                  worlds of projection mapping and theatre during his artistic
+                  path. Driven by an interest in hearing about people's
+                  hardships, resiliency, and aspirations. He is exploring
+                  projection mapping as a potent visual narrative technique to
+                  push the limits of storytelling even further. After exploring
+                  the field of projection mapping, Setthasiri came across a
+                  special method for fusing the real and virtual worlds,
+                  creating immersive settings that go beyond the limitations of
+                  theatre and film.
+                </p>
+              </div>
+            </div>
+          </div>
         </article>
 
         <article
@@ -113,12 +216,20 @@ const IndexPage = () => {
           </div>
           <div className="flex flex-col mt-5 gap-10">
             <Disclosure
-              head="Can I bring projector at 1st July ?"
+              head="Can I bring projectors at 1st July ?"
               body="Yes, you can !"
             />
             <Disclosure
-              head="What type of contents can I show ?"
-              body="Any type of contents are welcome ! (except NSFW). Only one restriction is sound, you can't play sound from your projector otherwise it will interfere with other projectors."
+              head="What types of content can I show ?"
+              body="Any types of content are welcome ! Except pornography and advertisement (commercial is ok as long as no big logo of brand on screen). Only one restriction is sound, you can't play sound from your projector otherwise it will interfere with other projectors."
+            />
+            <Disclosure
+              head="Can I come with many works ?"
+              body=" Yes, please. You can also be like the pavilion that gather your friends' work. You can beam as many works as you want."
+            />
+            <Disclosure
+              head="Can I just come with USB and/or harddisk ?"
+              body="Yes, you can. Just beware that your usb and harddisk is free of virus (otherwise this event will become `bring your own malware` ! we don't want to see that happens.), we can help you find some laptop that you can join to play along with the beam (but we cannot guarantee on this part). It's lot easier if you bring your own laptop."
             />
             <Disclosure
               head="Can I host my own BYOB Bangkok ?"
